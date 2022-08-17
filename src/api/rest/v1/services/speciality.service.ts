c1f86c46272be33201db2fae961a8cca06f50663
lpilas24 <leiva20152@gmail.com>
2022-08-17 16:21:21 -0500

    Added services to speciality controller

--- src/api/rest/v1/services/speciality.service.ts
+++ src/api/rest/v1/services/speciality.service.ts
@@ -5,4 +5,17 @@ const findAllSpecialities = async () => {
   return allSpecialities;
 };
 
-export { findAllSpecialities };
+const findSpeciality = async (props: any) => {
+  const speciality = Speciality.findOne({ ...props });
+  return speciality;
+};
+
+const saveSpeciality = async (props: any) => {
+  const speciality = new Speciality({
+    ...props,
+  });
+  const specialityDB = await speciality.save();
+  return specialityDB;
+};
+
+export { findAllSpecialities, findSpeciality, saveSpeciality };
